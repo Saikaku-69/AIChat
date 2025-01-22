e287073b284c2509c47fc4bd52b9bab1fe224ae0
Saikaku <grnmnsta@gmail.com>
2025-01-22 09:32:27 +0900

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by cmStudent on 2025/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
